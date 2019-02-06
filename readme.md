d31bafea028b9b3f8b44701b130726f3f512b2f6
Elliot Evans <ell15evans.nuls@googlemail.com>
2019-02-06 22:28:25 +0000

    commited latest

--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 # Presentation Framework
 
-
-WIP
+## Roadmap
+- Progress bar component
+- CSS Theming
+- page init and page destroy hooks
+- Ability to give it a number of pages
+- Ability to implement you own themes
+- Ability to add html content
--- src/main.js
+++ src/main.js
@@ -1,79 +1,221 @@
 const appShell = document.getElementById('app-shell');
 let isPageOneRegistered = false;
 let isPageTwoRegistered = false;
+const urls = {
+    prev: null,
+    current: null
+};
+const routeList = [
+    'pageOne',
+    'pageTwo'
+];
+const firstPage = routeList[0];
+const getNextPage = (currentRoute) => routeList[routeList.indexOf(currentRoute) + 1];
+const getPrevPage = (currentRoute) => routeList[routeList.indexOf(currentRoute) - 1];
+const setActivePage = (newUrl) => {
+    if(urls.current) {
+        urls.prev = urls.current;
+    }
+    urls.current = newUrl;
+};
+const toPascalCase = (word) => word.replace(/\w+/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase());
+const delimiterSeparateWord = (word) => word.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
+const isIndexRoute = () => location.pathname === '/' || location.pathname === null;
 
-document.getElementById('page1').addEventListener('click', () => {
+const routes = [
+    {
+        id: 1,
+        routeName: '',
+        routeClass: new class test {
+
+        },
+        isRegistered: false
+    }
+];
+
+if(isIndexRoute()) {
     while (appShell.firstChild) {
         appShell.removeChild(appShell.firstChild);
     }
 
     window.history.pushState(
-        {},
-        '/page1',
-        window.location.origin + '/page1'
+        setActivePage(firstPage),
+        `/${firstPage}`,
+        window.location.origin + `/${firstPage}`
     );
+}
 
+// go to next
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+        const currentPage = location.pathname.replace('/', '');
+        const nextPage = getNextPage(currentPage);
 
-    if(!isPageOneRegistered) {
-        class PageOne extends HTMLElement {
-            constructor() {
-                super();
-
-                const shadow = this.attachShadow({mode: 'open'});
-                const heading = document.createElement('h1');
-                const style = document.createElement('style');
-
-                heading.textContent = "PAGE ONE";
-                style.textContent = `
-                    :host {
-                        background: aqua;
-                        animation-duration: .38s;
-                        animation-fill-mode: both;
-                        animation-name: slideInUp;
-                    }
-                `;
-
-                shadow.appendChild(heading);
-                shadow.appendChild(style);
-            }
+        while (appShell.firstChild) {
+            appShell.removeChild(appShell.firstChild);
         }
 
-        customElements.define('page-1', PageOne, {extends: 'main'});
-        customElements.whenDefined('page-1').then(() => isPageOneRegistered = true);
+        window.history.pushState(
+            setActivePage(nextPage),
+            `/${nextPage}`,
+            window.location.origin + `/${nextPage}`
+        );
+
+        createPageComponent(nextPage);
     }
+    if (event.key === "ArrowLeft") {
+        const currentPage = location.pathname.replace('/', '');
+        const prevPage = getPrevPage(currentPage);
 
-    const pageOneElement = document.createElement('main', { is: 'page-1'});
-    appShell.appendChild(pageOneElement);
-});
-
-document.getElementById('page2').addEventListener('click', () => {
-    while (appShell.firstChild) {
-        appShell.removeChild(appShell.firstChild);
-    }
-
-    window.history.pushState(
-        {},
-        '/page2',
-        window.location.origin + '/page2'
-    );
-
-    if(!isPageTwoRegistered) {
-        class PageTwo extends HTMLElement {
-            constructor() {
-                super();
-
-                const shadow = this.attachShadow({mode: 'open'});
-                const heading = document.createElement('h1');
-
-                heading.textContent = "PAGE TWO";
-                shadow.appendChild(heading);
-            }
+        while (appShell.firstChild) {
+            appShell.removeChild(appShell.firstChild);
         }
 
-        customElements.define('page-2', PageTwo, {extends: 'main'});
-        customElements.whenDefined('page-2').then(() => isPageTwoRegistered = true);
-    }
+        window.history.pushState(
+            setActivePage(prevPage),
+            `/${prevPage}`,
+            window.location.origin + `/${prevPage}`
+        );
 
-    const pageTwoElement = document.createElement('main', { is: 'page-2'});
-    appShell.appendChild(pageTwoElement);
+        createPageComponent(prevPage);
+    }
 });
+
+const createPageComponent = (page) => {
+    console.log('CREATE COMPONENT');
+
+    const className = toPascalCase(page);
+    const elementName = delimiterSeparateWord(page);
+
+    console.log(className);
+    console.log(elementName);
+
+    const nameIt = (name, cls) => (
+        {
+            [name] : class extends cls {}
+        }
+    )[name];
+
+    const test123 = nameIt(className);
+    const t = new test123;
+
+    console.log(t);
+
+    // class className extends HTMLElement {
+    //     constructor() {
+    //         super();
+    //
+    //         this.setAttribute('id', 'page-1');
+    //
+    //         const shadow = this.attachShadow({mode: 'open'});
+    //         const heading = document.createElement('h1');
+    //         const style = document.createElement('style');
+    //
+    //         heading.textContent = "TEST PAGE";
+    //         style.textContent = `
+    //                 :host {
+    //                     background: red;
+    //                     animation-duration: .38s;
+    //                     animation-fill-mode: both;
+    //                     animation-name: slideInUp;
+    //                 }
+    //             `;
+    //
+    //         shadow.appendChild(heading);
+    //         shadow.appendChild(style);
+    //     }
+    // }
+
+    // customElements.define('page-1', className, {extends: 'main'});
+    // customElements.whenDefined('page-1').then(() => isPageOneRegistered = true);
+
+};
+
+// document.getElementById('page1').addEventListener('click', () => {
+    // while (appShell.firstChild) {
+    //     appShell.removeChild(appShell.firstChild);
+    // }
+
+    // window.history.pushState(
+    //     setActivePage('/page1'),
+    //     '/page1',
+    //     window.location.origin + '/page1'
+    // );
+
+//     if(!isPageOneRegistered) {
+//         class PageOne extends HTMLElement {
+//             constructor() {
+//                 super();
+//
+//                 this.setAttribute('id', 'page-1');
+//
+//                 const shadow = this.attachShadow({mode: 'open'});
+//                 const heading = document.createElement('h1');
+//                 const style = document.createElement('style');
+//
+//                 heading.textContent = "PAGE ONE";
+//                 style.textContent = `
+//                     :host {
+//                         background: aqua;
+//                         animation-duration: .38s;
+//                         animation-fill-mode: both;
+//                         animation-name: slideInUp;
+//                     }
+//                 `;
+//
+//                 shadow.appendChild(heading);
+//                 shadow.appendChild(style);
+//             }
+//         }
+//
+//         customElements.define('page-1', PageOne, {extends: 'main'});
+//         customElements.whenDefined('page-1').then(() => isPageOneRegistered = true);
+//     }
+//
+//     const pageOneElement = document.createElement('main', { is: 'page-1'});
+//     appShell.appendChild(pageOneElement);
+// });
+
+// document.getElementById('page2').addEventListener('click', () => {
+    // while (appShell.firstChild) {
+    //     appShell.removeChild(appShell.firstChild);
+    // }
+
+    // window.history.pushState(
+    //     setActivePage('/page2'),
+    //     '/page2',
+    //     window.location.origin + '/page2'
+    // );
+
+//     if(!isPageTwoRegistered) {
+//         class PageTwo extends HTMLElement {
+//             constructor() {
+//                 super();
+//
+//                 this.setAttribute('id', 'page-2');
+//
+//                 const shadow = this.attachShadow({mode: 'open'});
+//                 const heading = document.createElement('h1');
+//                 const style = document.createElement('style');
+//
+//                 heading.textContent = "PAGE TWO";
+//                 style.textContent = `
+//                     :host {
+//                         background: fuchsia;
+//                         animation-duration: .38s;
+//                         animation-fill-mode: both;
+//                         animation-name: slideInUp;
+//                     }
+//                 `;
+//                 shadow.appendChild(heading);
+//                 shadow.appendChild(style);
+//             }
+//         }
+//
+//         customElements.define('page-2', PageTwo, {extends: 'main'});
+//         customElements.whenDefined('page-2').then(() => isPageTwoRegistered = true);
+//     }
+//
+//     const pageTwoElement = document.createElement('main', { is: 'page-2'});
+//     appShell.appendChild(pageTwoElement);
+// });
