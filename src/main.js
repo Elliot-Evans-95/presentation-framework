a09a70761487176c69b19aeb645156d99d7baf13
Elliot Evans <ell15evans.nuls@googlemail.com>
2019-02-07 00:06:17 +0000

    implimented custom routing via html version

--- src/main.js
+++ src/main.js
@@ -1,221 +1,90 @@
+// ----- USER INPUT
+const routes = [
+    {
+        id: 1,
+        routeName: 'pageOne',
+        routeHTML: '<h1>HELLO WORLD</h1>',
+        routeStyle: '.test123 { background: aqua}',
+    },
+    {
+        id: 2,
+        routeName: 'pageTwo',
+        routeHTML: '<h1>HELLO WORLD 2</h1>',
+        routeStyle: '.test123 { background: aqua}',
+    }
+];
+
+//----- HELPERS
 const appShell = document.getElementById('app-shell');
-let isPageOneRegistered = false;
-let isPageTwoRegistered = false;
 const urls = {
     prev: null,
     current: null
 };
-const routeList = [
-    'pageOne',
-    'pageTwo'
-];
-const firstPage = routeList[0];
-const getNextPage = (currentRoute) => routeList[routeList.indexOf(currentRoute) + 1];
-const getPrevPage = (currentRoute) => routeList[routeList.indexOf(currentRoute) - 1];
+const firstPage = routes.length >= 0 ? routes[0].routeName : null;
+const getNextPage = (currentRoute) => {
+    const route = routes.find( (route) => route.routeName === currentRoute);
+    const currentIndex = route.id - 1;
+
+    return routes[currentIndex + 1] ? routes[currentIndex + 1].routeName : null;
+};
+const getPrevPage = (currentRoute) => {
+    const route = routes.find( (route) => route.routeName === currentRoute);
+    const currentIndex = route.id - 1;
+
+    return routes[currentIndex - 1] ? routes[currentIndex - 1].routeName : null;
+};
 const setActivePage = (newUrl) => {
     if(urls.current) {
         urls.prev = urls.current;
     }
     urls.current = newUrl;
 };
-const toPascalCase = (word) => word.replace(/\w+/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase());
-const delimiterSeparateWord = (word) => word.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
 const isIndexRoute = () => location.pathname === '/' || location.pathname === null;
+const getCurrentPage = () => location.pathname.replace('/', '');
+const setPageState = (page) => {
+    console.log(page);
 
-const routes = [
-    {
-        id: 1,
-        routeName: '',
-        routeClass: new class test {
-
-        },
-        isRegistered: false
-    }
-];
-
-if(isIndexRoute()) {
+    window.history.pushState(
+        setActivePage(page),
+        `/${page}`,
+        window.location.origin + `/${page}`
+    );
+};
+const addContentToPage = (page) => {
+    const pageRoute = routes.find( (route) => route.routeName === page);
+    const pageHTML = document.createElement('main');
+    pageHTML.innerHTML = pageRoute.routeHTML;
+    appShell.appendChild(pageHTML);
+};
+const removeContent = () => {
     while (appShell.firstChild) {
         appShell.removeChild(appShell.firstChild);
     }
-
-    window.history.pushState(
-        setActivePage(firstPage),
-        `/${firstPage}`,
-        window.location.origin + `/${firstPage}`
-    );
-}
-
-// go to next
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowRight") {
-        const currentPage = location.pathname.replace('/', '');
-        const nextPage = getNextPage(currentPage);
-
-        while (appShell.firstChild) {
-            appShell.removeChild(appShell.firstChild);
-        }
-
-        window.history.pushState(
-            setActivePage(nextPage),
-            `/${nextPage}`,
-            window.location.origin + `/${nextPage}`
-        );
-
-        createPageComponent(nextPage);
-    }
-    if (event.key === "ArrowLeft") {
-        const currentPage = location.pathname.replace('/', '');
-        const prevPage = getPrevPage(currentPage);
-
-        while (appShell.firstChild) {
-            appShell.removeChild(appShell.firstChild);
-        }
-
-        window.history.pushState(
-            setActivePage(prevPage),
-            `/${prevPage}`,
-            window.location.origin + `/${prevPage}`
-        );
-
-        createPageComponent(prevPage);
-    }
-});
-
-const createPageComponent = (page) => {
-    console.log('CREATE COMPONENT');
-
-    const className = toPascalCase(page);
-    const elementName = delimiterSeparateWord(page);
-
-    console.log(className);
-    console.log(elementName);
-
-    const nameIt = (name, cls) => (
-        {
-            [name] : class extends cls {}
-        }
-    )[name];
-
-    const test123 = nameIt(className);
-    const t = new test123;
-
-    console.log(t);
-
-    // class className extends HTMLElement {
-    //     constructor() {
-    //         super();
-    //
-    //         this.setAttribute('id', 'page-1');
-    //
-    //         const shadow = this.attachShadow({mode: 'open'});
-    //         const heading = document.createElement('h1');
-    //         const style = document.createElement('style');
-    //
-    //         heading.textContent = "TEST PAGE";
-    //         style.textContent = `
-    //                 :host {
-    //                     background: red;
-    //                     animation-duration: .38s;
-    //                     animation-fill-mode: both;
-    //                     animation-name: slideInUp;
-    //                 }
-    //             `;
-    //
-    //         shadow.appendChild(heading);
-    //         shadow.appendChild(style);
-    //     }
-    // }
-
-    // customElements.define('page-1', className, {extends: 'main'});
-    // customElements.whenDefined('page-1').then(() => isPageOneRegistered = true);
-
 };
 
-// document.getElementById('page1').addEventListener('click', () => {
-    // while (appShell.firstChild) {
-    //     appShell.removeChild(appShell.firstChild);
-    // }
+// ------- DOM
 
-    // window.history.pushState(
-    //     setActivePage('/page1'),
-    //     '/page1',
-    //     window.location.origin + '/page1'
-    // );
+if(isIndexRoute()) {
+    removeContent();
+    setPageState(firstPage);
+    addContentToPage(firstPage);
+}
 
-//     if(!isPageOneRegistered) {
-//         class PageOne extends HTMLElement {
-//             constructor() {
-//                 super();
-//
-//                 this.setAttribute('id', 'page-1');
-//
-//                 const shadow = this.attachShadow({mode: 'open'});
-//                 const heading = document.createElement('h1');
-//                 const style = document.createElement('style');
-//
-//                 heading.textContent = "PAGE ONE";
-//                 style.textContent = `
-//                     :host {
-//                         background: aqua;
-//                         animation-duration: .38s;
-//                         animation-fill-mode: both;
-//                         animation-name: slideInUp;
-//                     }
-//                 `;
-//
-//                 shadow.appendChild(heading);
-//                 shadow.appendChild(style);
-//             }
-//         }
-//
-//         customElements.define('page-1', PageOne, {extends: 'main'});
-//         customElements.whenDefined('page-1').then(() => isPageOneRegistered = true);
-//     }
-//
-//     const pageOneElement = document.createElement('main', { is: 'page-1'});
-//     appShell.appendChild(pageOneElement);
-// });
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+        const currentPage = getCurrentPage();
+        const nextPage = getNextPage(currentPage);
 
-// document.getElementById('page2').addEventListener('click', () => {
-    // while (appShell.firstChild) {
-    //     appShell.removeChild(appShell.firstChild);
-    // }
+        removeContent();
+        setPageState(nextPage);
+        addContentToPage(nextPage);
+    }
+    if (event.key === "ArrowLeft") {
+        const currentPage = getCurrentPage();
+        const prevPage = getPrevPage(currentPage);
 
-    // window.history.pushState(
-    //     setActivePage('/page2'),
-    //     '/page2',
-    //     window.location.origin + '/page2'
-    // );
-
-//     if(!isPageTwoRegistered) {
-//         class PageTwo extends HTMLElement {
-//             constructor() {
-//                 super();
-//
-//                 this.setAttribute('id', 'page-2');
-//
-//                 const shadow = this.attachShadow({mode: 'open'});
-//                 const heading = document.createElement('h1');
-//                 const style = document.createElement('style');
-//
-//                 heading.textContent = "PAGE TWO";
-//                 style.textContent = `
-//                     :host {
-//                         background: fuchsia;
-//                         animation-duration: .38s;
-//                         animation-fill-mode: both;
-//                         animation-name: slideInUp;
-//                     }
-//                 `;
-//                 shadow.appendChild(heading);
-//                 shadow.appendChild(style);
-//             }
-//         }
-//
-//         customElements.define('page-2', PageTwo, {extends: 'main'});
-//         customElements.whenDefined('page-2').then(() => isPageTwoRegistered = true);
-//     }
-//
-//     const pageTwoElement = document.createElement('main', { is: 'page-2'});
-//     appShell.appendChild(pageTwoElement);
-// });
+        removeContent();
+        setPageState(prevPage);
+        addContentToPage(prevPage);
+    }
+});
