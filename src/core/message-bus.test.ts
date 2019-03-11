32d4cfeb6a7c180119fac5646db75c1ca1f1fb79
Elliot Evans <ell15evans.nuls@googlemail.com>
2019-03-11 22:31:58 +0000

    added another text

---
+++ src/core/message-bus.test.ts
@@ -0,0 +1,41 @@
+export class MessageBus {
+    public subscriptions = {};
+
+    constructor() {}
+
+    public subscribe(eventType, callback) {
+        const id = this.getIdGenerator();
+
+        if(!this.subscriptions[eventType])
+            this.subscriptions[eventType] = {};
+
+        this.subscriptions[eventType][id] = callback;
+
+        return {
+            unsubscribe: () => {
+                delete this.subscriptions[eventType][id];
+
+                if(Object.keys(this.subscriptions[eventType]).length === 0) delete this.subscriptions[eventType];
+            }
+        }
+    }
+
+    public publish(eventType, arg) {
+        if(!this.subscriptions[eventType]) return;
+
+        Object.keys(this.subscriptions[eventType])
+            .forEach(key => this.subscriptions[eventType][key](arg))
+    }
+
+    public getIdGenerator() {
+        let lastId = 0;
+
+        return function getNextUniqueId() {
+            lastId += 1;
+
+            return lastId;
+        }
+    }
+}
+
+export const messageBus = new MessageBus();
