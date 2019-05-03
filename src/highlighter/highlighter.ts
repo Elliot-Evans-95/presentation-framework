3997fc81251545072efc60396cae144382e42206
Elliot Evans <ell15evans.nuls@googlemail.com>
2019-05-03 01:43:38 +0100

    suppressing errors in imprort files for highlight.js as it does work

--- src/highlighter/highlighter.ts
+++ src/highlighter/highlighter.ts
@@ -1,5 +1,7 @@
+// @ts-ignore
 import hljs from 'highlight.js/lib/highlight';
 import 'highlight.js/styles/tomorrow.css';
+// @ts-ignore
 import typescript from 'highlight.js/lib/languages/typescript';
 import {Names} from "../slide/dom.type";
 import {DocumentWrapper} from "../documentWrapper/document-wrapper";
